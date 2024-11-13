7f05f430a356b75f73aaf855c0ab7e81ff027b39
Muhammad Ahsan <ahsanwebengr@gmail.com>
2024-11-13 23:22:50 +0500

    fix: change status code of reviews

--- src/controllers/admin.js/testimonial.js
+++ src/controllers/admin.js/testimonial.js
@@ -1,16 +1,16 @@
-import Testimonials from "../../models/testimonial.js";
-import { catchAsync } from "../../middleware/utils.js";
-import testimonial from "../../models/testimonial.js";
+import Testimonials from '../../models/testimonial.js';
+import { catchAsync } from '../../middleware/utils.js';
+import testimonial from '../../models/testimonial.js';
 
 //create testimonial
 export const createTestimonial = catchAsync(async (req, res) => {
   let check = await Testimonials.find({ name: req.body.name });
   if (check.length > 0) {
-    return res.status(409).json({ error: "testimonial Already Exists" });
+    return res.status(409).json({ error: 'testimonial Already Exists' });
   }
 
   if (!req.file) {
-    return res.status(400).json({ error: "Image is required" });
+    return res.status(400).json({ error: 'Image is required' });
   }
 
   req.body.image = req.file.filename;
@@ -24,11 +24,9 @@ export const createTestimonial = catchAsync(async (req, res) => {
 export const getTestimonial = catchAsync(async (req, res) => {
   const testimonial = await Testimonials.findById(req.params.id);
   if (!testimonial) {
-    return res.status(404).json({ error: "testimonial Not Found" });
+    return res.status(404).json({ error: 'testimonial Not Found' });
   }
-  res
-    .status(200)
-    .json({ message: "testimonial Get Successfully", testimonial });
+  res.status(200).json({ message: 'testimonial Get Successfully', testimonial });
 });
 
 //getTestimonials
@@ -48,36 +46,34 @@ export const getTesimonials = catchAsync(async (req, res) => {
     testimonials: testimonial,
   };
 
-  return res
-    .status(200)
-    .json({ data, message: "testimonials Get Successfully" });
+  return res.status(200).json({ data, message: 'testimonials Get Successfully' });
 });
 
 //deleteTestimonial
 export const deleteTestimonial = catchAsync(async (req, res) => {
   const testimonial = await Testimonials.findByIdAndDelete(req.params.id);
   if (!testimonial) {
-    return res.status(404).json({ error: "testimonial Not Found" });
+    return res.status(404).json({ error: 'testimonial Not Found' });
   }
-  res.status(200).json({ message: "testimonial Deleted Successfully" });
+  res.status(200).json({ message: 'testimonial Deleted Successfully' });
 });
 
 //delete all testimonials
 export const deleteAllTestimonials = catchAsync(async (req, res) => {
   await Testimonials.deleteMany({});
-  res.status(200).json({ message: "All testimonials Deleted Successfully" });
+  res.status(200).json({ message: 'All testimonials Deleted Successfully' });
 });
 
 //updateTestimonial
 export const updateTestimonial = catchAsync(async (req, res) => {
   const testimonialId = req.params.id;
 
-  let  testimonial = Testimonials.findById(testimonialId)
+  let testimonial = Testimonials.findById(testimonialId);
 
-  if (!req.file) {
-    req.body.image = testimonial.image
-  }else{
+  if (req.file) {
     req.body.image = req.file.filename;
+  } else {
+    req.body.image = testimonial.image;
   }
 
   const updatedTestimonial = await Testimonials.findByIdAndUpdate(
@@ -89,7 +85,7 @@ export const updateTestimonial = catchAsync(async (req, res) => {
   );
 
   if (!updatedTestimonial) {
-    return res.status(404).json({ error: "testimonial not found" });
+    return res.status(404).json({ error: 'Testimonial not found' });
   }
-  res.status(200).json({ message: "testimonial Updated Successfully" });
+  res.status(201).json({ message: 'Testimonial Updated Successfully' });
 });
